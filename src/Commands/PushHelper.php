2d49d8d9c88fb603321990f8950828556b0cf20d
Toby Zerner <toby.zerner@gmail.com>
2016-11-16 15:53:41 +1030

    Add commit + push helpers

---
+++ src/Commands/PushHelper.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Hyn\GitHelpers\Commands;
+
+use Hyn\GitHelpers\Objects\Directory;
+use Hyn\GitHelpers\Objects\Package;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PushHelper extends Command
+{
+    /**
+     * @var Directory
+     */
+    protected $directory;
+
+    protected function configure()
+    {
+        $this->setName('push')
+            ->setDescription('Push changes for repositories in all subdirectories.')
+            ->addArgument('match', InputArgument::OPTIONAL, 'Only pull matching directories [optional, regex]');
+
+        $this->directory = new Directory();
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeLn(["<comment>Loading directories to pull for: {$this->directory->getPath()}</comment>", '']);
+
+        if ($this->directory->getSubdirectories()->isEmpty()) {
+            $output->writeln('<error>No subdirectories found.</error>');
+
+            return;
+        }
+
+        // Loop through all directories to search for repositories.
+        foreach ($this->directory->getSubdirectories() as $subDirectory) {
+            $quotedMatch = preg_quote(rtrim($input->getArgument('match'), DIRECTORY_SEPARATOR));
+            if ($input->getArgument('match') && !preg_match("/$quotedMatch/", $subDirectory)) {
+                $output->writeln("<comment>Skipped {$subDirectory}</comment>");
+                continue;
+            }
+
+            // Instantiates package from directory.
+            $package = new Package(
+                $subDirectory
+            );
+
+            $output->writeln(sprintf('<info>%s - version: %s</info>', $package->name, $package->latestTag));
+
+            $package->push();
+
+            // Return to original path.
+            chdir($this->directory->getCurrentPath());
+        }
+    }
+}
